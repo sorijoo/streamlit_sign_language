2330ee96eb492bbec93eeab8f497739ac7cee844
EunJida <107987493+EunJida@users.noreply.github.com>
2022-07-22 17:40:50 +0900

    Create 3_💡_Kor_Progress.py

---
+++ pages/3_💡_Kor_Progress.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+from html_module import line_break, section, callout, title
+from PIL import Image
+
+# 전체 페이지 설정
+st.set_page_config(
+    page_title="Korean Sign Number Progress",
+    page_icon="☝️",
+    initial_sidebar_state="expanded", 
+    menu_items={
+        'Get Help': 'https://www.extremelycoolapp.com/help',
+        'Report a bug': "https://www.extremelycoolapp.com/bug",
+        'About': "# This is a header. This is an *extremely* cool app!"
+    }
+)
+
+title('a')
